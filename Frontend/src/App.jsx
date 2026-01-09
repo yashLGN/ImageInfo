ce1997c55e7b593bcd4988f5dfc7cfc04215b364
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-01-10 00:43:18 +0530

    backend connected address given

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     const formData = new FormData();
     formData.append("image", image);
 
-    const response = await fetch("http://127.0.0.1:5000/upload", {
+    const response = await fetch("https://imageinfo.onrender.com/upload", {
       method: "POST",
       body: formData,
     });
